e41ff65cd65c666a14019d88811e0803dc00222b
Athena Funderburg <hiden64@protonmail.com>
2026-03-10 06:06:55 +0000

    make it look less cramped

--- ServiceVault/wtv-head-waiter/login.js
+++ ServiceVault/wtv-head-waiter/login.js
@@ -23,7 +23,7 @@ data = `<html>
     
 <tr>
     <td rowspan=2 width=184 valign=top align=left>
-        <img src="ROMCache/Spacer.gif" width=184 height=63><br>
+        <img src="ROMCache/Spacer.gif" width=164 height=63><br>
         <img src="ROMCache/Spacer.gif" width=21>
         <img src="ROMCache/WebTVLogoJewel.gif">
     
@@ -37,14 +37,19 @@ data = `<html>
         <img src="ROMCache/Spacer.gif" width=1 height=1>
         
 <tr>
-    <td colspan=2  width=120 height=312  valign=top align=left bgcolor=000000>
+    <td colspan=2 width=120 height=312 valign=top align=left bgcolor=000000>
+        <table width=305 cellspacing=0 cellpadding=0>
+            <tr><td>
 <b>
-WebTV Redialed no longer operates at the server your WebTV reciever is currently connecting to.
+WebTV Redialed no longer operates at the server your reciever is configured to connect to.
 </b>
 <p>
-You will need to update your routes. <br> If you use DreamPi to connect, re-flash your Raspberry Pi with our latest DreamPi image. <br> If you are using a landline or VoIP telephone connection, unplug your receiver, plug it back in, and press "Moved".
+You will need to update your routes. <br><br>If you use DreamPi to connect, re-flash your Raspberry Pi with our latest DreamPi image. <br><br>If you are using a landline or VoIP telephone connection, unplug your receiver, plug it back in, and press "Moved".
 <p>
-If you have any further questions, join our Discord server. <BR> It is linked on our web site: http://webtv.zone/
+If you have any further questions, join our Discord server, which is linked on our web site: http://webtv.zone/
+            </td></tr>
+        </table>
+</td>
 </table>
     
 </body>
